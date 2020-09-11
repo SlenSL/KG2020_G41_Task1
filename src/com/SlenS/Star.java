a806822054fc8be1d146235de9a945aab52d5825
Andrew Budaev <55806194+SlenSL@users.noreply.github.com>
2020-09-12 00:37:56 +0300

    Add files via upload

--- src/com/SlenS/Star.java
+++ src/com/SlenS/Star.java
@@ -7,16 +7,16 @@ class Star implements Drawable {
     int y;
 
 
-
     public Star(int x, int y) {
-        this.x = (int) ( Math.random() * x );
-        this.y = (int) ( Math.random() * y );
+        this.x = (int) (Math.random() * x);
+        this.y = (int) (Math.random() * y);
     }
 
     @Override
     public void draw(Graphics2D g) {
         g.setColor(Color.WHITE);
-        g.fillOval(x , y, (int) Math.random()*2+3, (int) Math.random()*2+3);
+        int d = (int) (Math.random() * 2 + 3);
+        g.fillArc(x, y, d, d, 0, 360);
     }
 
 
